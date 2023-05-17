2d511562cb2d5d1427bbc6adfc6ae2e88f8119e6
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-17 14:15:54 +0000

    props/components

---
+++ episode1/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={120000} position="Junior SDE" company="Google" />
+      <Job salary={150000} position="Project Manager" company="Netflix" />
+    </div>
+  );
+}
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  );
+};
+
+export default App;
