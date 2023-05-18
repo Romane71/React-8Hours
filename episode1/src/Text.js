632b3727e01c723a5d109e7b35cbc85cefbfb681
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-18 17:06:50 +0000

    component lifecycle, useEffect hook

---
+++ episode1/src/Text.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export const Text = () => {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("COMPONENT IS MOUNTED");
+
+    return () => {};
+  }, [text]);
+
+  return (
+    <div>
+      <input
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
+      <h1>{text}</h1>
+    </div>
+  );
+};
